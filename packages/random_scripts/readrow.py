06f364f2f517827ade76787b2a8d986bb30f3291
Chaitanya Ghone <csghone@acceletrade.com>
2017-10-12 10:15:34 +0530

    Minor updates
    Signed-off-by: Chaitanya Ghone <csghone@acceletrade.com>

--- packages/random_scripts/readrow.py
+++ packages/random_scripts/readrow.py
@@ -4,11 +4,15 @@ import sys
 
 filename   = sys.argv[1]
 search_str = sys.argv[2]
+delim      = sys.argv[3]
+
+if delim == "":
+    delim = ","
 
 file = open(filename, "r")
 header_line = file.readline()
 
-headers = header_line.strip().split(",")
+headers = header_line.strip().split(delim)
 
 match=False
 for line in file:
@@ -20,7 +24,6 @@ for line in file:
 if not match:
     print("Cannot find", search_str)
 else:
-    values = line.split(",")
+    values = line.split(delim)
     for index in range(len(headers)):
         print(index+1, headers[index], values[index])
-
