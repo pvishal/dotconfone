c4d8f77902af602c0e5535e1c18d53f925487683
Chaitanya Ghone <csghone@acceletrade.com>
2017-10-26 03:24:18 +0530

    New java
    Signed-off-by: Chaitanya Ghone <csghone@acceletrade.com>

--- packages/ubuntu/scripts/install_packages_java.sh
+++ packages/ubuntu/scripts/install_packages_java.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ $1 == "install" ]; then
-    sudo -E apt-get install -y openjdk-8-jdk
-    sudo -E apt-get install -y openjdk-8-jre libxmu6 libpangoxft-1.0-0 libpangox-1.0-0 libxv1
-    sudo -E apt-get install -y openjdk-8-jre:i386 libxmu6:i386 libpangoxft-1.0-0:i386 libpangox-1.0-0:i386 libxv1:i386
-    sudo -E apt-get install -y icedtea-8-plugin
+    sudo -E apt-get install -y openjdk-9-jdk
+    sudo -E apt-get install -y openjdk-9-jre libxmu6 libpangoxft-1.0-0 libpangox-1.0-0 libxv1
+    sudo -E apt-get install -y openjdk-9-jre:i386 libxmu6:i386 libpangoxft-1.0-0:i386 libpangox-1.0-0:i386 libxv1:i386
+    sudo -E apt-get install -y icedtea-9-plugin
 fi
