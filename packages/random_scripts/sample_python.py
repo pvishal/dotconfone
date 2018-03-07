a14e30156d9d55100eb60b7aa8dc8e857f70b289
Chaitanya Ghone <csghone@acceletrade.com>
2018-03-07 10:03:28 +0530

    Update python sample
    Signed-off-by: Chaitanya Ghone <csghone@acceletrade.com>

--- packages/random_scripts/sample_python.py
+++ packages/random_scripts/sample_python.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 eg:
-./sample_python.py -x opt1 -y 3 -z 1 2 3
+./sample_python.py -w -x opt1 -y 3 -z 1 2 3
 
 """
 from __future__ import print_function
@@ -21,10 +21,9 @@ LOG_FORMATTER = logging.Formatter(
 def setup_logging(level=logging.INFO):
     file_log_handler = logging.handlers.RotatingFileHandler(
         "__" + os.path.basename(__file__) + ".main__" + ".log",
-        maxBytes = 1000000,
-        backupCount = 5)
+        maxBytes=1000000,
+        backupCount=5)
     console_log_handler = logging.StreamHandler()
-    logger = logging.getLogger()
     logger.addHandler(file_log_handler)
     logger.addHandler(console_log_handler)
     logger.setLevel(level)
@@ -32,17 +31,21 @@ def setup_logging(level=logging.INFO):
         handler.setFormatter(fmt=LOG_FORMATTER)
 
 
+def print_variables(**kwargs):
+    for key, value in kwargs.items():
+        print(key, value)
+
+
 def process(**kwargs):
+    wval = kwargs.get("wval", False) # Set default values if needed
     xval = kwargs["xval"]
     yval = kwargs["yval"]
     zval = kwargs["zval"]
 
-    print(xval)
-    print(yval)
-    print(zval)
+    print_variables(**kwargs)
 
-    logger.debug("debug")
-    logger.info("info")
+    logger.debug("Message: %s", "debug")
+    logger.info("Message: %s, %s", "info", 1)
     logger.warning("warning")
     logger.error("error")
     logger.critical("critical")
@@ -53,12 +56,20 @@ def process(**kwargs):
 def main():
     parser = argparse.ArgumentParser(description="Generic Application")
     parser.add_argument(
+        "-w",
+        "--wval",
+        dest="wval",
+        action="store_true",
+        help="This variable is set to True only when '-w' is given",
+        default=False
+    )
+    parser.add_argument(
         "-x",
         "--xval",
         dest="xval",
         choices=["opt1", "opt2"],
         type=str.lower, # Make case-insensitive
-        help="Some Argument",
+        help="Case-insensitive String Argument",
         required=True
     )
     parser.add_argument(
@@ -66,7 +77,7 @@ def main():
         "--yval",
         dest="yval",
         choices=[3, 4],
-        help="Some Argument",
+        help="Numerical Arguments",
         type=int,
         default=4,
         required=True
@@ -76,7 +87,7 @@ def main():
         "--zval",
         dest="zval",
         nargs="+",
-        help="Some Argument",
+        help="Array arguments",
         type=int,
         default=[]
     )
@@ -88,4 +99,4 @@ def main():
 
 if __name__ == '__main__':
     setup_logging(level=logging.INFO)
-    sys.exit(main())
+    sys.exit(main()) # Send return value to shell
